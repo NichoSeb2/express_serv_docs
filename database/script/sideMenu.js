a2f3d6f7984b0ac891337507fe16c06b1356bd3f
NichoSeb2 <NichoSeb2@outlook.fr>
2021-02-13 15:43:38 +0100

    added database sample file

---
+++ database/script/sideMenu.js
@@ -0,0 +1,187 @@
+/**
+ * Created by Kupletsky Sergey on 17.10.14.
+ *
+ * Material Sidebar (Profile menu)
+ * Tested on Win8.1 with browsers: Chrome 37, Firefox 32, Opera 25, IE 11, Safari 5.1.7
+ * You can use this sidebar in Bootstrap (v3) projects. HTML-markup like Navbar bootstrap component will make your work easier.
+ * Dropdown menu and sidebar toggle button works with JQuery and Bootstrap.min.js
+ */
+
+// Sidebar toggle
+//
+// -------------------
+$(document).ready(function() {
+    var overlay = $('.sidebar-overlay');
+
+    $('.sidebar-toggle').on('click', function() {
+        var sidebar = $('#sidebar');
+        sidebar.toggleClass('open');
+        if ((sidebar.hasClass('sidebar-fixed-left') || sidebar.hasClass('sidebar-fixed-right')) && sidebar.hasClass('open')) {
+            overlay.addClass('active');
+        } else {
+            overlay.removeClass('active');
+        }
+    });
+
+    $('.sidebar-tgl').on('click', function() {
+        var sidebar = $('#sidebar');
+        sidebar.toggleClass('open');
+        if ((sidebar.hasClass('sidebar-fixed-left') || sidebar.hasClass('sidebar-fixed-right')) && sidebar.hasClass('open')) {
+            overlay.addClass('active');
+        } else {
+            overlay.removeClass('active');
+        }
+    });
+
+    overlay.on('click', function() {
+        $(this).removeClass('active');
+        $('#sidebar').removeClass('open');
+    });
+
+    $("tbody tr:odd").addClass("even");
+
+    var index = 0,max = 0;
+    $(".search").keyup(function() {
+        max = 0;
+        searchedText = $('.search').val();
+        console.log('text :' + searchedText)
+        $(".search-found").removeClass("search-found");
+        $("p:contains('"+searchedText+"')," + 
+                "td:contains('"+searchedText+"')"
+            ).each( function( i, element ) {
+            max++;
+            var content = $(element).text();
+            content = content.replace( searchedText, '<span class="search-found">' + searchedText + '</span>' );
+            $(element).html( content );
+        });
+    });
+
+    $("#btnNext").click(function() {
+        $('html, body').animate({
+            scrollTop: $($(".search-found")[index]).offset().top - 80
+        }, 2000);
+        if( index < max - 1) {index++}
+            else{ index = 0} 
+        console.log('index value' + index);
+    });
+
+    
+    
+});
+
+$(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top - 80
+            }, 1000);
+            return false;
+          }
+        }
+      });
+});
+
+// Sidebar constructor
+//
+// -------------------
+$(document).ready(function() {
+
+    var sidebar = $('#sidebar');
+    var sidebarHeader = $('#sidebar .sidebar-header');
+    var sidebarImg = sidebarHeader.css('background-image');
+    var toggleButtons = $('.sidebar-toggle');
+
+    // Hide toggle buttons on default position
+    toggleButtons.css('display', 'none');
+    $('body').css('display', 'table');
+
+    // Sidebar position
+    $('#sidebar-position').change(function() {
+        var value = $( this ).val();
+        sidebar.removeClass('sidebar-fixed-left sidebar-fixed-right sidebar-stacked').addClass(value).addClass('open');
+        if (value == 'sidebar-fixed-left' || value == 'sidebar-fixed-right') {
+            $('.sidebar-overlay').addClass('active');
+        }
+        // Show toggle buttons
+        if (value != '') {
+            toggleButtons.css('display', 'initial');
+            $('body').css('display', 'initial');
+        } else {
+            // Hide toggle buttons
+            toggleButtons.css('display', 'none');
+            $('body').css('display', 'table');
+        }
+    });
+
+    // Sidebar theme
+    $('#sidebar-theme').change(function() {
+        var value = $( this ).val();
+        sidebar.removeClass('sidebar-default sidebar-inverse sidebar-colored sidebar-colored-inverse').addClass(value)
+    });
+
+    // Header cover
+    $('#sidebar-header').change(function() {
+        var value = $(this).val();
+
+        $('.sidebar-header').removeClass('header-cover').addClass(value);
+
+        if (value == 'header-cover') {
+            sidebarHeader.css('background-image', sidebarImg)
+        } else {
+            sidebarHeader.css('background-image', '')
+        }
+    });
+
+    sidebar.removeClass('sidebar-default sidebar-inverse sidebar-colored sidebar-colored-inverse').addClass('sidebar-colored')
+    sidebar.removeClass('sidebar-fixed-left sidebar-fixed-right sidebar-stacked').addClass(sidebar-stacked).addClass('open');
+   });
+
+/**
+ * Created by Kupletsky Sergey on 08.09.14.
+ *
+ * Add JQuery animation to bootstrap dropdown elements.
+ */
+
+(function($) {
+    var dropdown = $('.dropdown');
+
+    // Add slidedown animation to dropdown
+    dropdown.on('show.bs.dropdown', function(e){
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    });
+
+    // Add slideup animation to dropdown
+    dropdown.on('hide.bs.dropdown', function(e){
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    });
+})(jQuery);
+
+
+
+(function(removeClass) {
+
+    jQuery.fn.removeClass = function( value ) {
+		if ( value && typeof value.test === "function" ) {
+			for ( var i = 0, l = this.length; i < l; i++ ) {
+				var elem = this[i];
+				if ( elem.nodeType === 1 && elem.className ) {
+					var classNames = elem.className.split( /\s+/ );
+
+					for ( var n = classNames.length; n--; ) {
+						if ( value.test(classNames[n]) ) {
+							classNames.splice(n, 1);
+						}
+					}
+					elem.className = jQuery.trim( classNames.join(" ") );
+				}
+			}
+		} else {
+			removeClass.call(this, value);
+		}
+		return this;
+	}
+
+})(jQuery.fn.removeClass);
